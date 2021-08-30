fbb94d3a64c2584112d273d56e882cd3b7bf7e44
td-bn <bnabi@protonmail.com>
2021-08-30 12:55:43 +0530

    Refactor, test on mumbai testnet

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -18,7 +18,8 @@ export function Gallery({URIs, provider, contract}) {
         const URI = URIs[i]
         let {data} = await axios.get(URI)
         const owner = await contract.ownerOf(i)
-        data = {owner, ...data}
+        const approved = await contract.getApproved(i)
+        data = {owner, approved, ...data}
         console.log('data ',data)
         tokenInfo.push(data)
       }
@@ -27,7 +28,7 @@ export function Gallery({URIs, provider, contract}) {
       setSignerAddress(signer.getAddress ? await signer.getAddress() : '')
     }
     init()    
-  }, [URIs])
+  }, [URIs, loading])
 
   const onBuy = async (e, i, owner) => {
     setLoading(true);
@@ -40,7 +41,10 @@ export function Gallery({URIs, provider, contract}) {
     try {
       const tx = await contract.connect(signer).buy(i)
       const receipt = await tx.wait()
+      window.alert('You now own the NFT')
     } catch (error) {
+      if (error.code === 4001)
+        window.alert('User rejected transaction')
       console.error(error)
     }
     setLoading(false);
@@ -57,8 +61,12 @@ export function Gallery({URIs, provider, contract}) {
 
     try {
       const tx = await contract.connect(signer).approve(contract.address, i)
+      console.log(tx)
       const receipt = await tx.wait()
+      window.alert('Approved')
     } catch (error) {
+      if (error.code === 4001)
+        window.alert('User rejected approval')
       console.error(error)
     }
     setLoading(false);
@@ -70,20 +78,26 @@ export function Gallery({URIs, provider, contract}) {
       <div className="px-lg-5">
         <div className="row">
           {
-            tokenInfo.map( (itmeInfo, i) => (
+            tokenInfo.map( (itemInfo, i) => (
               <div key={i} className="col-xl-5 col-lg-5 col-md-6 mb-5" style={{
                 margin: "4%"
               }}>
-                <p>Owner <small style={{fontSize: 12}}>{itmeInfo.owner}</small></p>
-                <div className="bg-white rounded shadow-sm"><img src={itmeInfo.image} alt="" className="img-fluid card-img-top" />
+                <p>Owner <small style={{fontSize: 12}}>{itemInfo.owner}</small></p>
+                <div className="bg-white rounded shadow-sm"><img src={itemInfo.image} alt="" className="img-fluid card-img-top" />
                   <div className="p-4">
-                    <h5>{itmeInfo.name}</h5>
-                    <p className="small text-muted mb-0">{itmeInfo.description}</p>
+                    <h5>{itemInfo.name}</h5>
+                    <p className="small text-muted mb-0">{itemInfo.description}</p>
                     <div className="d-flex align-items-center justify-content-between rounded-pill bg-light px-3 py-2 mt-4">
-                      {signerAddress === itmeInfo.owner ? 
+                      {signerAddress === itemInfo.owner && ethers.constants.AddressZero === itemInfo.approved? 
                         <button className="btn btn-light" onClick={(e) => onApprove(e, i)}>Approve for sale</button> : ''
                       }
-                      <button className="btn btn-light" onClick={(e) => onBuy(e, i, itmeInfo.owner)}>Buy</button>
+                      {
+                        itemInfo.owner === signerAddress 
+                        ? <button className="btn btn-light" disabled>You own this NFT</button>
+                        : itemInfo.approved != ethers.constants.AddressZero ?
+                          <button className="btn btn-light" onClick={(e) => onBuy(e, i, itemInfo.owner)}>Buy</button>
+                          : <button className="btn btn-light" disabled>Item not approved for sale</button>
+                      }
                     </div>
                   </div>
                 </div>
